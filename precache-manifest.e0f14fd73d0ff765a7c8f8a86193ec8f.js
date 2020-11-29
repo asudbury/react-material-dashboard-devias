c8416124f3fc98edc88d97c93642a785ecee9d67
asudbury <sudburya@gmail.com>
2020-11-29 16:10:18 +0000

    Deploying to gh-pages from  @ 2a8c5dd0f7e81d1b943955b85a4a02a2b48ff9f2 🚀

--- precache-manifest.e0f14fd73d0ff765a7c8f8a86193ec8f.js
+++ precache-manifest.e0f14fd73d0ff765a7c8f8a86193ec8f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1ff699f8e6090e2634dbbc2fd691acf",
+    "revision": "ab48d6b5aa24a75fa69ffcb093fd9ea0",
     "url": "/react-material-dashboard-devias/index.html"
   },
   {
-    "revision": "7b313171cd797c629135",
+    "revision": "3023767499a74ce7985a",
     "url": "/react-material-dashboard-devias/static/css/2.18fb2ca0.chunk.css"
   },
   {
-    "revision": "7b313171cd797c629135",
-    "url": "/react-material-dashboard-devias/static/js/2.1bee4d29.chunk.js"
+    "revision": "3023767499a74ce7985a",
+    "url": "/react-material-dashboard-devias/static/js/2.76a58978.chunk.js"
   },
   {
     "revision": "6553848ede33e774934d8afdf700acf9",
-    "url": "/react-material-dashboard-devias/static/js/2.1bee4d29.chunk.js.LICENSE.txt"
+    "url": "/react-material-dashboard-devias/static/js/2.76a58978.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b5eff9f9d7e22e9f5335",
-    "url": "/react-material-dashboard-devias/static/js/main.82b926d5.chunk.js"
+    "revision": "b876e92eb4d2f3f63d45",
+    "url": "/react-material-dashboard-devias/static/js/main.4f3ec8f0.chunk.js"
   },
   {
     "revision": "1ef2aafd545e2e007c87",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-material-dashboard-devias/precache-manifest.af0cee519187e27fa2fd8079b582261e.js"
+  "/react-material-dashboard-devias/precache-manifest.e0f14fd73d0ff765a7c8f8a86193ec8f.js"
 );
 
 self.addEventListener('message', (event) => {
