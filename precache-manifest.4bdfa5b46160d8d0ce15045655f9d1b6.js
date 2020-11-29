1d3765c2fbba9d66d8bdd37519771f933a2c3c53
asudbury <sudburya@gmail.com>
2020-11-29 15:34:49 +0000

    Deploying to gh-pages from  @ c2039c6e89ff108f18f61970417501a30dfa4bd7 🚀

---
+++ precache-manifest.4bdfa5b46160d8d0ce15045655f9d1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b80fb4b6ababceffd76be25375c91d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91d253f84a0aaea8ccab",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "91d253f84a0aaea8ccab",
+    "url": "/static/js/2.7afdff1b.chunk.js"
+  },
+  {
+    "revision": "6553848ede33e774934d8afdf700acf9",
+    "url": "/static/js/2.7afdff1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c5290b5b60cd0b8db3",
+    "url": "/static/js/main.33cbb479.chunk.js"
+  },
+  {
+    "revision": "c946e3509499ac77ec3c",
+    "url": "/static/js/runtime-main.6baf1c9c.js"
+  }
+]);
