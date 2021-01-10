b468aa0106bc510f027d06bf24bef7f9357a2c70
Adrian Sudbury <sudburya@gmail.com>
2021-01-10 17:19:42 +0000

    Remove redundant files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render((
@@ -9,5 +8,3 @@ ReactDOM.render((
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
-
-serviceWorker.unregister();
